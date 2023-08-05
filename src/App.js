8fc5fa57396c511e9f4a6d0f4e379124855d8d76
fuseini abdul wahab <abdulwahabfuseini78@gmail.com>
2023-08-05 04:21:58 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ Aos.init();
 
 const App = () => {
   return (
-    <Router basename="/Rest-Assured">
+    <Router>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Rest-Assured" element={<Home />} />
         <Route path="about-us" element={<AboutUs />} />
         <Route path="gallery" element={<Gallery />} />
         <Route path="review" element={<Review />} />
